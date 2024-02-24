9cad3f1618f8e2af80055c361f192548d76670bb
Daniel Souza <dassilved@gmail.com>
2024-02-24 10:36:10 -0300

    add ignore & configs flap.py

---
+++ flap.py
@@ -0,0 +1,22 @@
+import pygame
+import os
+import random
+
+# Configuração tela
+TELA_LARGURA = 500
+TELA_ALTURA = 800
+
+# Configuração assets
+IMAGEM_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe.png')))
+IMAGEM_CHAO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base.png')))
+IMAGEM_FUNDO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))
+IMAGENS_PASSARO = [ 
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird1.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird3.png')))
+    ]
+
+# Configuração fontes
+pygame.font.init()
+FONTE_PONTOS = pygame.font.SysFont('arial', 50)
+
